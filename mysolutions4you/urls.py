f4ff245ee0b856458b45fa00bab865c6945103cb
Harun-or-Rashid <harunducse23rd@gmail.com>
2020-09-06 11:19:43 +0600

    make it better

--- mysolutions4you/urls.py
+++ mysolutions4you/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('blog.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+
+urlpatterns =urlpatterns+ path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),)
